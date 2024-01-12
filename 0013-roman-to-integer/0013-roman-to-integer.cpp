add0cf56166bd6068d4fd1b4f65f426c00246b1e
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-01-12 21:44:45 +0530

    Time: 16 ms (28.04%), Space: 9.8 MB (6.6%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int prior(char ch){
+        if(ch=='I') return 1;
+        if(ch=='V') return 5;
+        if(ch=='X') return 10;
+        if(ch=='L') return 50;
+        if(ch=='C') return 100;
+        if(ch=='D') return 500;
+        if(ch=='M') return 1000;
+        return 0;
+    }
+    int romanToInt(string s) {
+        stack<int>st;
+        int su=0;
+        for(int i=0;i<s.size();i++){
+            if(st.empty() || prior(s[i])<=prior(st.top())){
+                st.push(s[i]);
+            }
+            else{
+                su+=prior(s[i])-prior(st.top());
+                //st.push(prior(s[i])-prior(st.top()));
+                st.pop();
+                //st.push(prior(s[i])-an);
+            }
+        }
+        //int su=0;
+        while(!st.empty()){
+            su+=prior(st.top());
+            st.pop();
+        }
+        return su;
+    }
+};
