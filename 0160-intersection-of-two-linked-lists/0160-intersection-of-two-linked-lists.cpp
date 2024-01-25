fe2a146b00b2259a481d61b0910edfc965a6c6f5
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-01-25 17:29:06 +0530

    Time: 41 ms (46.36%), Space: 17 MB (27.22%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int ha=0,hb=0;
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        while(temp1!=NULL){
+            ha++;
+            temp1=temp1->next;
+        }
+        while(temp2!=NULL){
+            hb++;
+            temp2=temp2->next;
+        }
+        int diff=abs(ha-hb);
+        if(ha>hb){
+            while(diff!=0){
+                diff--;
+                headA=headA->next;
+            }
+        }
+        else{
+            while(diff!=0){
+                diff--;
+                headB=headB->next;
+            }
+        }
+        //cout<<maxi<<" "<<ha<<" "<<hb;
+        while(headA!=NULL){
+            if(headA==headB){
+                return headA;
+            }
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
