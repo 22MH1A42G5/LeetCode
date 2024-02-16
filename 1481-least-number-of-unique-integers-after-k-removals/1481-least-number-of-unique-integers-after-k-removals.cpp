8908db2e2c999e8f678bde3e0b8554676b243a95
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-02-16 21:00:36 +0530

    Time: 161 ms (49.83%), Space: 66.2 MB (51.33%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         for(int i = 0 ; i < arr.size() ; i ++){
             mp[arr[i]]++;
         }
         vector<int> v;
         for( auto it : mp){
             v.push_back(it.second);
-        }
-        for(int i = 0 ; i < v.size() ; i ++){
-            cout<<v[i]<<" ";
         }
         sort(v.begin(),v.end());
         int i=0;
