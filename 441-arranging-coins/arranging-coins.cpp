eb59d1d1ff0174d304dccc101415de986f5808b9
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-06-01 16:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (48.26%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        double s = n;
+        double te = 1+8*s;
+        double ans = (-1 + sqrt(te))/2;
+        int f = ans;
+        return ans;
+    }
+};
