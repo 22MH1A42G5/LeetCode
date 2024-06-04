96065b5ab0bc416a5057442923bd11193107b649
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-06-04 19:12:48 +0530

    Time: 6 ms (16.33%) | Memory: 9 MB (48.94%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        map<char,int>mp;
-        for(int i = 0 ; i < s.size() ; i++){
-            mp[s[i]]++;
+        map<int,int>mpp;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            mpp[s[i]]++;
         }
-        int c = 0 , ans = 0;
-        for(auto it:mp){
-            if(it.second%2==0){
-                ans+=it.second;
-            }else{
-                if(it.second > 1){
-                    ans+=it.second-1;
-                    c=1;
-                }
-                else
-                c=1;
+        int even=0;
+        int odd=0;
+        int sin=0;
+        int ans=0;
+        int maxi=0;
+        int temp = 0;
+        // if(n == mpp.size()) return 1;
+        for(auto it:mpp)
+        {
+            if(it.second%2==0)
+            {
+                even+=it.second;
+            }
+            else if(it.second%2==1)
+            {
+                odd+=it.second-1;
+                temp=1;
             }
         }
-        return ans+c;
+        ans=even+odd+temp;
+        // if(ans%2==0)
+        // {
+        //     for(auto it:mpp)
+        //     {
+        //         if(it.second==1)
+        //         {
+        //             ans+=1;
+        //             break;
+        //         }
+        //     }
+        // }
+        if(ans == 0) return 1;
+        return ans;
     }
 };
