30a71739a4cf502d2bef4c51e04935239e12df0f
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-07-19 11:42:59 +0530

    Time: 16 ms (56.65%), Space: 14.3 MB (18.75%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& mat) {
-        vector<int>minis;
-        vector<int>maxis;
-        for(int i=0;i<mat.size();i++){
-            int min=mat[i][0];
-            for(int j=0;j<mat[0].size();j++){
-                if(min>mat[i][j]){
-                    min=mat[i][j];
-                }
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        vector<int>rows;
+        vector<int>cols;
+        vector<int>res;
+        int mx,mn,n=matrix.size(),m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            mn=100001;
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]<mn)
+                    mn=matrix[i][j];
             }
-            minis.push_back(min);
+            rows.push_back(mn);
         }
-        for(int i=0;i<mat[0].size();i++){
-            int max=0;
-            for(int j=0;j<mat.size();j++){
-                if(max<mat[j][i]){
-                    max=mat[j][i];
-                }
+        for(int i=0;i<m;i++){
+            mx=0;
+            for(int j=0;j<n;j++){
+                if(matrix[j][i]>mx)
+                    mx=matrix[j][i];
             }
-            auto t=find(minis.begin(),minis.end(),max);
-            if(std::find(minis.begin(), minis.end(), max) != minis.end()){
-                maxis.clear();
-                maxis.push_back(max);
-                return maxis;
-            }
-            maxis.push_back(max);
+            cols.push_back(mx);
         }
-        
-        return {};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==cols[j] && matrix[i][j]==rows[i])
+                    res.push_back(cols[j]);
+            }
+        }
+        return res;
     }
 };
