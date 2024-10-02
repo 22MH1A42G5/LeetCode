6dbc64e192ebc729ba73481e1d5ecd1711ce713e
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-10-02 22:58:51 +0530

    Time: 208 ms (5.34%) | Memory: 44.1 MB (41.66%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        map<int,int>mp;
+        for(int i = 0 ; i < arr.size() ; i++){
+            mp[arr[i]]=1;
+        }
+        vector<int>ans;
+        auto i = 1;
+        for(auto it : mp){
+            // cout<<it.first<<" "<<it.second<<" "<<i<<endl;
+            mp[it.first] = i;
+            // cout<<it.first<<" "<<it.second<<" "<<endl;
+            i++;
+        }
+        for(int i = 0 ; i < arr.size() ; i++){
+            cout<<mp[arr[i]]<<endl;
+        }
+        for(int i = 0 ; i < arr.size() ; i++){
+            ans.push_back(mp[arr[i]]);
+        }
+        return ans;
+    }
+};
