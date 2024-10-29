bc9d2a58b1b22ef8794dcd7b587369efdf3d1559
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2024-10-29 23:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.64%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>big;
+        for(long long i=0;i<numRows;i++){
+            long long n=i+1;
+            vector<int>ls;
+            for(long long j=0;j<n;j++){
+                if(j==0 || j==n-1){
+                    ls.push_back(1);
+                    }
+                else{
+                    ls.push_back(big[i-1][j-1]+big[i-1][j]);
+                }
+            }
+            big.push_back(ls);
+        }
+        return big;
+    }
+};
