bc9fe6388d20ac07c714a8f686eda921bf3b3a48
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2024-12-24 23:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (32.54%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int binaryToDecimal(string& str) {
+        int n = 0;
+        for (char ch : str) {
+            n <<= 1;
+            if (ch == '1') {
+                n += 1;
+            } else if (ch != '0') {
+                return -1;
+            }
+        }
+        return n;
+    }
+    int smallestNumber(int n) {
+        string s = "";
+        while(n > 0){
+            s += "1";
+            n >>= 1;
+        }
+        return binaryToDecimal(s);
+    }
+};
