2badcfc407749c389f42724fc996f168a3ac8d2f
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2024-12-27 22:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (24.68%) - LeetSync

---
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& v) {
+        int n = v.size();
+        vector<int> maxL(n);
+        maxL[0] = v[0];
+        int maxS = 0;
+        for (int i = 1; i < n; i++) {
+            int right = v[i] - i;
+            maxS = max(maxS, maxL[i - 1] + right);
+            int left = v[i] + i;
+            maxL[i] = max(maxL[i - 1], left);
+        }
+        return maxS;
+    }
+};
