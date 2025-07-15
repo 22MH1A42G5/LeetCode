5847791792edb05123a049decc2386edc12babb2
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-07-15 13:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.14%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     bool isValid(string word) {
-        int v=0 , c=0 , d = 0;
-        if(word.size() < 3){
-            return false;
-        }
-        for(int i = 0 ; i < word.size() ; i++){
-            if(word[i]=='a' || word[i]=='e' || word[i]=='i' || word[i]=='o' || word[i]=='u' || word[i]=='A' || word[i]=='E' || word[i]=='I' || word[i]=='O' || word[i]=='U'){
-                v=1;
-            }
-            else if((word[i]>='a' && word[i]<='z') || (word[i]>='A' && word[i]<='Z')){
-                d=1;
-            }
-            else if(word[i]>= '0' && word[i]<='9'){
-                // just leave
+        string vow = "aeiouAEIOU";
+        int vf = 0 , cf = 0;
+        if(word.size() < 3)
+        return false;
+        for(int i = 0 ;i < word.size(); i++){
+            if((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z') || (word[i] >= '0' && word[i] <= '9')){
+                if(vow.find(word[i]) != string::npos){
+                    vf = 1;
+                }
+                if(vow.find(word[i]) == string::npos && (word[i] < '0' || word[i] > '9')){
+                    cf = 1;
+                }
             }
             else{
+                cout << word[i] << " ";
                 return false;
             }
         }
-        if(v==1 && d==1)
+        cout << vf << " " << cf << "\n";
+        if(vf == 1 && cf == 1){
             return true;
-        return false;
+        }
+        else{
+            return false;
+        }
     }
 };
