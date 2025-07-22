30b54821c39bad4269d046ac7dbfbb471cc516f0
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-07-22 21:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.36%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,7 +15,16 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1 , -1);
-        return fun(nums , n-1 , dp);
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0] , nums[1]);
+        vector<int>dp(n , -1);
+        // return fun(nums , n-1 , dp);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0]+nums[2];
+        for(int i = 3 ; i < n ; i++){
+            dp[i] = max(dp[i-2] + nums[i],dp[i-3] + nums[i]);
+        }
+        return max(dp[n-2] , dp[n-1]);
     }
 };
