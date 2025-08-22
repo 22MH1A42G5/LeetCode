381b9e8b5cebab97e3bbc9b9f04c718cd800bcc8
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-08-22 11:28:34 +0530

    Time: 655 ms (5.11%) | Memory: 13.3 MB (35.69%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -20,7 +20,31 @@ public:
     }
     int minDistance(string word1, string word2) {
         int n = word1.size() , m = word2.size();
-        vector<vector<int>>dp(n , vector<int>(m , -1));
-        return EditDistance(word1.size()-1 , word2.size()-1 , word1 , word2 , dp);
+        // vector<vector<int>>dp(n , vector<int>(m , -1));
+        // return EditDistance(word1.size()-1 , word2.size()-1 , word1 , word2 , dp);
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+        for(int i = 0 ;i <= n ;i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0 ; j <= m ; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1 ; i <= word1.size() ; i++){
+            for(int j = 1 ; j <= word2.size() ; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j] , min(dp[i][j-1] , dp[i-1][j-1]));
+                }
+            }
+        }
+        for(int i = 0 ; i <= word1.size();  i++){
+            for(int j = 0 ; j <= word2.size() ; j++){
+                cout << dp[i][j] <<" ";
+            }
+            cout << "\n";
+        }
+        return dp[n][m];
     }
 };
