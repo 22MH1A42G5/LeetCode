8d671819b041add4d6852d8e0fc7c0e451d1efb4
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-12-02 15:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (63.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& pr) {
-        int i = 0 , j = 1;
-        int ans = 0;
-        while(i < (pr.size() - 1)){
-            if(pr[i] < pr[i+1]){
-                ans += pr[i+1] - pr[i];
+    int maxProfit(vector<int>& prices) {
+        int buying_stock_price = prices[0];
+        int profit = 0;
+        for(int i = 0 ; i < prices.size() ; i++){
+            if(buying_stock_price < prices[i]){
+                profit += prices[i] - buying_stock_price;
             }
-            i++;
+            buying_stock_price = prices[i];
         }
-        return ans;
+        return profit;
     }
 };
+// 7 1 5 3 6 4
+
+
+// 7
+// 5
+// 3
+// 7 1 5 3 6 4
+
+// profit: 4 + 3
