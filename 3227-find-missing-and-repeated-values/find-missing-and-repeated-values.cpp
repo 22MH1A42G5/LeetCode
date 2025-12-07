bd6b7ab644e28f3be7cde78947f88d706cba1bca
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-12-07 20:47:38 +0530

    Time: 3 ms (67.15%) | Memory: 25.4 MB (97.06%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,20 +1,60 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        map<int,int>mp;
-        for(int i = 0 ; i < grid.size() ; i++){
-            for(int j = 0 ; j < grid[0].size() ; j++){
-                mp[grid[i][j]]++;
+    bool setbit(int xor_val, int num) {
+        int msb = -1;
+
+        for (int bit = 31; bit >= 0; bit--) {
+            int mask = 1 << bit;
+            if (xor_val & mask) {
+                msb = bit;
+                break;
             }
         }
-        int te = grid.size()*grid.size();
-        int fir , la;
-        for(int i = 1 ; i <= te;i++){
-            if(mp[i] == 2)
-            fir = i;
-            if(mp[i] == 0)
-            la = i;
+
+        if (msb == -1) return false;
+
+        int mask = 1 << msb;
+        return (num & mask) != 0;
+    }
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int xor_var = 0;
+        int n = grid.size();
+        n = n*n;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[i].size() ; j++){
+                xor_var ^= grid[i][j];
+            }
         }
-        return {fir,la};
+        for(int i = 0 ; i < n ; i++){
+            xor_var ^= (i+1);
+        }
+        int dup = 0 , mis = 0;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[i].size() ; j++){
+                if(setbit(xor_var , grid[i][j])){
+                    dup ^= grid[i][j];
+                }
+                else{
+                    mis ^= grid[i][j];
+                }
+            }
+        }
+        for(int i = 1 ; i <= n ; i++){
+            if(setbit(xor_var , i)){
+                dup ^= i;
+            }
+            else{
+                mis ^= i;
+            }
+        }
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[i].size() ; j++){
+                if(mis == grid[i][j]){
+                    swap(dup , mis);
+                    break;
+                }
+            }
+        }
+        return {dup , mis};
     }
 };
