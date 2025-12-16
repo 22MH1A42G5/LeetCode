5062899881170d7ba91340850dfe6e5b540c7cde
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-12-16 22:17:51 +0530

    Time: 4 ms (17.64%), Space: 10.6 MB (11.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str="";
+        for(int i = 0 ; i < s.size() ; i++){
+            if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')){
+                if(s[i] >= '0' && s[i] <= '9'){
+                    str += s[i];
+                }
+                else{
+                    str += tolower(s[i]);
+                }
+            }
+        }
+        string temp = str;
+        reverse(temp.begin() , temp.end());
+        return (str == temp);
+    }
+};
