9b7e10161539395f741d6160aecc0070f1fd6048
KAMPARAPU SRI RAM <kamparapusriram@gmail.com>
2025-12-16 22:17:49 +0530

    Time: 4 ms (17.64%) | Memory: 10.6 MB (11.59%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string st="";
-        for(int i = 0 ; i < s.size(); i++ ){
-            int ch=int(s[i]);
-            if((ch >= 65 and ch<=90) || (ch >= 97 and ch <= 122) || (ch >= 48 and ch<=57)){
-
-                st+=(tolower(s[i]));
+        string str="";
+        for(int i = 0 ; i < s.size() ; i++){
+            if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9')){
+                if(s[i] >= '0' && s[i] <= '9'){
+                    str += s[i];
+                }
+                else{
+                    str += tolower(s[i]);
+                }
             }
         }
-        string st1=st;
-        reverse(st1.begin(),st1.end());
-        if(st1==st)
-        return true;
-        else 
-        return false;
+        string temp = str;
+        reverse(temp.begin() , temp.end());
+        return (str == temp);
     }
 };
